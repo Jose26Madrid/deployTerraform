bd568090b70bd5a612c7144284468c412a21a764
sr_ramiro@hotmail.es <sr_ramiro@hotmail.es>
2025-04-18 00:10:05 +0200

    conf pipeline.

--- scripts/clone_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Clonando repositorio"
-[ ! -d ~/terraform ] && git clone https://github.com/Jose26Madrid/terraform.git ~/terraform || echo "El proyecto ya existe, no se clona"
-ls -lrt terraform
